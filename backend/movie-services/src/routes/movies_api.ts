fbde5d86321d192d608f3035d3a1de7896401b9e
Lars Skifjeld <larsski@ntnu.no>
2021-10-15 14:32:47 +0200

    #9 Create route at root and fetch all movies

--- backend/movie-services/src/routes/movies_api.ts
+++ backend/movie-services/src/routes/movies_api.ts
@@ -1,18 +1,16 @@
-// (app) => {
-//     console.log("movesApi created");
-    
-//     app.get(`/api/`, async (req: any, res: any) => {
-//         const URL = "https://imdb-api.com/en/API/250Movies"
-//         const API_KEY = "k_o72ahudx"
-//         try {
-//             const movies = await fetch(URL, {
-//                 headers: new Headers({
-//                     apiKey: API_KEY,    
-//                 })
-//             })
-//         } catch (err) {
-//             console.log(err);
-            
-//         }
-//     })
-// }
+const express = require("express")
+const QueryModel = require("../models/queryModel.ts")
+
+const router = express.Router();
+router.get("/", async ( req, res ) => {
+    try {
+        let queryResult = await QueryModel.find({})
+        console.log("queryResult", queryResult);
+        res.send( queryResult )
+    } catch (error) {
+        console.log(error);
+        res.send( error );
+    }
+});
+
+module.exports = router;
