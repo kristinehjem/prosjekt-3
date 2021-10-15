442afb3ccdfbe93e42bad738986d9da292a23a0f
Lars Skifjeld <larsski@ntnu.no>
2021-10-15 14:30:15 +0200

    Connect to database with mongoose and refactor routes

---
+++ backend/movie-services/src/models/queryModel.ts
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+const FetchMoviesSchema = new mongoose.Schema({Object});
+
+const FetchMovies = mongoose.model('posts', FetchMoviesSchema)
+module.exports = FetchMovies;
