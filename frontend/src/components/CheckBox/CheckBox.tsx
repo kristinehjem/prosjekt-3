835c25c239fd20640cf9a087aabc8c21b43f3fa3
Jørgen Usterud Myrvold <jorgeum@stud.ntnu.no>
2021-10-18 11:07:40 +0200

    cleaned up checkbox component

--- frontend/src/components/CheckBox/CheckBox.tsx
+++ frontend/src/components/CheckBox/CheckBox.tsx
@@ -1,26 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 
-export default function CheckBox(props: {label: string}) {
-    const [state, setState] = React.useState({
-        epoch: false
-    });
+export default function CheckBox(props: { label: string }) {
+  const [checked, setState] = React.useState(false);
 
-const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState({
-        ...state,
-        [event.target.name]: event.target.checked,
-    });
-    };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState(!checked);
+  };
 
-const {epoch} = state;
-
-return (
+  return (
     <FormControlLabel
-        control={
-            <Checkbox sx={{color: 'white'}} checked={epoch} onChange={handleChange} name="epoch" color='default'/>
-        }
-        label= {props.label}
-        />)
+      control={
+        <Checkbox
+          sx={{ color: "white" }}
+          checked={checked}
+          onChange={handleChange}
+          name="epoch"
+          color="default"
+        />
+      }
+      label={props.label}
+    />
+  );
 }
