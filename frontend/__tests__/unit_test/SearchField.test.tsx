385bf08291afd9cba2cd81400d4aa1893261156a
Jørgen Usterud Myrvold <jorgeum@stud.ntnu.no>
2021-10-20 09:16:29 +0200

    added searchField test file and id for searchField

---
+++ frontend/__tests__/unit_test/SearchField.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+test("Seach field updates state", () => {
+  const setSearch = jest.fn((value) => {});
+  const { queryByPlaceholderText } = render(
+    <SearchBox setSearch={setSearch} />
+  );
+
+  const searchInput = queryByPlaceholderText("Search...");
+
+  fireEvent.change(searchInput, { target: { value: "test" } });
+
+  expect(searchInput.value).toBe("test");
+});
--- frontend/src/components/SearchField/SearchField.tsx
+++ frontend/src/components/SearchField/SearchField.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react';
+import * as React from "react";
 import SearchBar from "material-ui-search-bar";
 import "../SearchField/SearchField.css";
 
 export default function SearchField() {
-  const [state, seState] = React.useState()
-  
-    return (
-      <div className="searchbar">
+  const [state, seState] = React.useState();
+
+  return (
+    <div className="searchbar">
       <SearchBar
-        value = {state}
+        value={state}
         placeholder="Search for movie title"
+        aria-label="search-bar"
       />
     </div>
   );
