e2c876ff1bf5e997313c6cf052b50d895f879bed
Lars Skifjeld <larsski@ntnu.no>
2021-10-20 14:30:33 +0200

    #12 Add pagination to filmgrid

--- frontend/src/components/FilmGrid/FilmGrid.tsx
+++ frontend/src/components/FilmGrid/FilmGrid.tsx
@@ -2,6 +2,7 @@ import "./FilmGrid.css";
 import Grid from '@mui/material/Grid';
 import FilmCard from '../FilmCard/FilmCard'
 import { useQuery, gql } from '@apollo/client';
+import { ReactChild, useEffect, useState } from 'react';
 
 // apollo with typescript: https://www.apollographql.com/docs/react/development-testing/static-typing/
 interface Movie {
@@ -45,32 +46,43 @@ query getMoviesByYear($year: String, $offset: Int, $limit: Int)  {
 `
 
 export default function FilmGrid() {
+  const [page, setPage] = useState<number>(1);
+  const [filmCards, setFilmCards] = useState<ReactChild[]>()
+  const itemPerPage = 2;
   //const movieQuery = useQuery<MoviesList>(GET_MOVIES);
 
-  const { loading, error, data } = useQuery<MoviesList>(GET_MOVIES_YEAR, {
-    variables: { offset: 0, limit: 2, year: "2014" },
+  const {data} = useQuery<MoviesList>(GET_MOVIES_YEAR, {
+    variables: { offset: (page - 1) * itemPerPage, limit: itemPerPage, year: "2014" },
   });
+  useEffect(() => {
 
-  let filmCards;
-  if (data !== undefined && data !== null) {
-    const movies = Object.values(data)[0] || []
-    filmCards = movies.map((movie: Movie) =>
-      (<div key={movie.id} className="film">
-        <FilmCard
-          title={movie.title}
-          year={movie.year}
-          pictureURL={movie.image}
-          rating={movie.imdbRating}
-          rank={movie.rank} />
-      </div>));
-  } else {
-    filmCards = (<div>loading...</div>)
-  }
+    // alert(page)
+    if (data !== undefined && data !== null) {
+      const movies = Object.values(data)[0] || []
+      setFilmCards(movies.map((movie: Movie) =>
+        <div key={movie.id} className="film">
+          <FilmCard
+            title={movie.title}
+            year={movie.year}
+            pictureURL={movie.image}
+            rating={movie.imdbRating}
+            rank={movie.rank} />
+        </div>));
+    }
+  }, [data])
   return (
     <div id="filmgrid-wrapper">
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className="filmGrid">
         {filmCards}
       </Grid>
+      {!filmCards &&
+        <div>
+          Loading...
+        </div>
+      }
+      <button type="button" onClick={() => { setPage(page - 1)}}>Forrige side</button>
+      <button type="button" onClick={() => { setPage(page + 1)}}>Neste side</button>
+
     </div>
   )
 }
