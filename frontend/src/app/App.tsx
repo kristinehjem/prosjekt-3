a162fc08305a0f6213cd0912e526ef9cb5f288e7
krishjem <10qg5423LD>
2021-10-25 13:38:26 +0200

    added missing file changes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -27,7 +27,7 @@ function App() {
             <div className="sidebar">
               <SideBar></SideBar>
             </div>
-            <div className="filmgrid">
+            <div className="moviegrid">
               <MovieGrid></MovieGrid>
             </div>
           </div>
